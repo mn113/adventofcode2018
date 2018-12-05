152417f402220d7fdbaf7343813f83db133e57f5
Martin Nicholson <recyclebing+github@gmail.com>
2018-12-05 19:57:47 +0100

    Finished Day 05

--- php/Day05.php
+++ php/Day05.php
@@ -1,15 +1,52 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: martin
- * Date: 05/12/2018
- * Time: 18:31
- */
+<?php namespace AOC\php;
 
-namespace AOC\php;
+use \AOC\php\Solution;
+use \AOC\php\TwoParter;
 
+class Day05 extends Solution implements TwoParter {
 
-class Day05
-{
+    public $data;
+
+    function __construct() {
+        $this->input = trim(file('../inputs/05.txt')[0]);
+    }
+
+    function sameApartFromCase($a, $b) {
+        return (strtoupper($a) == $b && strtolower($b) == $a) ||
+                (strtoupper($b) == $a && strtolower($a) == $b);
+    }
+
+    function reduce($input) {
+        $reduced = $input;
+        $pattern1 = '/(aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ)/';
+        $pattern2 = '/(Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz)/';
+        while (preg_match($pattern1, $reduced) || preg_match($pattern2, $reduced)) {
+            $reduced = preg_replace([$pattern1, $pattern2], '', $reduced);
+        }
+        return $reduced;
+    }
+
+    function part1() {
+        // Find resulting length after replacement loop:
+        echo strlen($this->reduce($this->input));
+    }
+
+    function part2() {
+        $results = [];
+        // Process whole alphabet:
+        foreach (str_split('abcdefghijklmnopqrstuvwxyz') as $a) {
+            $string = $this->input;
+            // Strip char:
+            echo "$a ";
+            $string = preg_replace("/$a/", '', $string);
+            $string = preg_replace("/".strtoupper($a)."/", '', $string);
+            $length = strlen($this->reduce($string));
+            $results[$a] = $length;
+            echo "$length ";
+        }
+        // Find min:
+        asort($results);
+        echo "\nShortest: " . $results[array_keys($results)[0]];
+    }
 
 }
